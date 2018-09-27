f9f602f18b239f36dd8be70c60e67643ed8a7f12
Yan Makouski <makovskiy.yan@gmail.com>
2018-09-27 03:22:24 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  let currLength = 1;
+  let longest = 0;
+  let obj = new Set(array);//make object Set to check there elements 
+  
+  for (let i of array) {  //some ES6 magic for iterate through array elements
+    if (!obj.has(i - 1)) { //if Set does not have lower element than current we need to start count from this element;
+      let c = i; // we make a dublicate for our element in case)))
+      currLength = 1;//before every countiong should be 1!
+      while (obj.has(c + 1)) {//while we have next element increase our length
+        currLength += 1;
+        c += 1;
+      }
+      longest = Math.max(longest,currLength); // choose the longest one and assign it to longest
+
+    }
+  }
+  return array.length == 1 ? 1 : longest;
 }
